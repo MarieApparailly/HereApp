dbc93b04aaf89882be69018e0c204532ac484d1e
MarieApparailly <mapparailly@ensc.fr>
2023-04-23 23:55:31 +0200

    V1 fonctionelle : maps + geolocation + multi users

--- navigation/RootTabNavigator.js
+++ navigation/RootTabNavigator.js
@@ -1,89 +1,90 @@
-import React, {useState, useEffect} from 'react';
-import {Platform, Text, View, StyleSheet} from 'react-native';
+// import React, {useState, useEffect} from 'react';
+// import {Platform, Text, View, StyleSheet} from 'react-native';
 
-import * as Location from 'expo-location';
+// import * as Location from 'expo-location';
 
-export default function RootTabNavigator() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+// export default function RootTabNavigator() {
+//   const [location, setLocation] = useState(null);
+//   const [errorMsg, setErrorMsg] = useState(null);
 
-  useEffect(() => {
-    (async () => {
-      let {status} = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
+//   useEffect(() => {
+//     (async () => {
+//       let {status} = await Location.requestForegroundPermissionsAsync();
+//       if (status !== 'granted') {
+//         setErrorMsg('Permission to access location was denied');
+//         return;
+//       }
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
+//       let location = await Location.getCurrentPositionAsync({});
+//       setLocation(location);
+//     })();
+//   }, []);
 
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
+//   let text = 'Waiting..';
+//   if (errorMsg) {
+//     text = errorMsg;
+//   } else if (location) {
+//     text = JSON.stringify(location);
+//   }
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>{text}</Text>
-    </View>
-  );
-}
-// import React from 'react';
-// import {StatusBar, StyleSheet} from 'react-native';
-// // import Ionicons from 'react-native-vector-icons/Ionicons';
-// import {NavigationContainer} from '@react-navigation/native';
-// import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-// import UsersMapStackNavigator from './UsersMapStackNavigator';
-// import UsersListStackNavigator from './UsersListStackNavigator';
-// // import {setStatusBarBackgroundColor} from 'expo-status-bar';
-
-// const Tab = createBottomTabNavigator();
-
-// const RootTabNavigator = () => {
 //   return (
-//     <NavigationContainer>
-//       {/* <StatusBar barStyle="light-content" backgroundColor="#f4511e" /> */}
-//       <Tab.Navigator
-//         screenOptions={({route}) => ({
-//           // Icons will be different if the tab is focused
-//           // tabBarStyle: {backgroundColor: '#f4511e'},
-//           //   tabBarIcon: ({focused, color, size}) => {
-//           //     const icons = {
-//           //       UsersListStack: 'ios-list-outline',
-//           //       UsersMapStack: 'ios-map',
-//           //     };
-//           //     return (
-//           //       <Ionicons
-//           //         name={icons[route.name] || 'ios-menu'}
-//           //         size={25}
-//           //         color={color}
-//           //       />
-//           //     );
-//           //   },
-//           // Hiding tab navigator header to show only stack header
-//           headerShown: false,
-//         })}>
-//         <Tab.Screen
-//           name="UsersListStack"
-//           component={UsersListStackNavigator}
-//           options={{title: 'Status'}}
-//         />
-//         <Tab.Screen
-//           name="UsersMapStack"
-//           component={UsersMapStackNavigator}
-//           options={{title: 'Position'}}
-//         />
-//       </Tab.Navigator>
-//     </NavigationContainer>
+//     <View style={styles.container}>
+//       <Text style={styles.paragraph}>{text}</Text>
+//     </View>
 //   );
-// };
+// }
 
-// export default RootTabNavigator;
+import React from 'react';
+import {StatusBar, StyleSheet} from 'react-native';
+// import Ionicons from 'react-native-vector-icons/Ionicons';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import UsersMapStackNavigator from './UsersMapStackNavigator';
+import UsersListStackNavigator from './UsersListStackNavigator';
+// import {setStatusBarBackgroundColor} from 'expo-status-bar';
+
+const Tab = createBottomTabNavigator();
+
+const RootTabNavigator = () => {
+  return (
+    <NavigationContainer>
+      {/* <StatusBar barStyle="light-content" backgroundColor="#f4511e" /> */}
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          // Icons will be different if the tab is focused
+          // tabBarStyle: {backgroundColor: '#f4511e'},
+          //   tabBarIcon: ({focused, color, size}) => {
+          //     const icons = {
+          //       UsersListStack: 'ios-list-outline',
+          //       UsersMapStack: 'ios-map',
+          //     };
+          //     return (
+          //       <Ionicons
+          //         name={icons[route.name] || 'ios-menu'}
+          //         size={25}
+          //         color={color}
+          //       />
+          //     );
+          //   },
+          // Hiding tab navigator header to show only stack header
+          headerShown: false,
+        })}>
+        <Tab.Screen
+          name="UsersListStack"
+          component={UsersListStackNavigator}
+          options={{title: 'Status'}}
+        />
+        <Tab.Screen
+          name="UsersMapStack"
+          component={UsersMapStackNavigator}
+          options={{title: 'Position'}}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RootTabNavigator;
 
 const styles = StyleSheet.create({
   container: {
--- screens/UsersMapScreen.js
+++ screens/UsersMapScreen.js
@@ -1,35 +1,16 @@
-// import {
-//   SafeAreaView,
-//   View,
-//   Text,
-//   StyleSheet,
-//   StatusBar,
-//   Image,
-//   Button,
-// } from 'react-native';
-
-// const RootTabNavigator = () => {
-
-//   //   return (
-//   //     <SafeAreaView style={{flex: 1}}>
-
-//   //     </SafeAreaView>
-//   //   );
-// };
-
-import React, {useState, useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
   TouchableOpacity,
   StyleSheet,
   PermissionsAndroid,
   Platform,
+  Text,
   View,
+  Button,
 } from 'react-native';
-
-//import all the components we are going to use.
-import Geolocation from '@react-native-community/geolocation';
-
+import * as TaskManager from 'expo-task-manager';
+import * as Location from 'expo-location';
 import MapView, {Marker} from 'react-native-maps';
 import UsersListScreen from './UsersListScreen';
 
@@ -38,110 +19,124 @@ const UsersMapScreen = ({navigation}) => {
     return <UsersListScreen />;
   };
 
-  const [currentLongitude, setCurrentLongitude] = useState('...');
-  const [currentLatitude, setCurrentLatitude] = useState('...');
-  const [locationStatus, setLocationStatus] = useState('');
+  const LOCATION_TASK_NAME = 'LOCATION_TASK_NAME';
+  let foregroundSubscription = null;
 
-  useEffect(() => {
-    const requestLocationPermission = async () => {
-      if (Platform.OS === 'ios') {
-        getOneTimeLocation();
-        subscribeLocationLocation();
-      } else {
-        try {
-          const granted = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-            {
-              title: 'Location Access Required',
-              message: 'This App needs to Access your location',
-            },
-          );
-          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-            //To Check, If Permission is granted
-            getOneTimeLocation();
-            subscribeLocationLocation();
-          } else {
-            setLocationStatus('Permission Denied');
-          }
-        } catch (err) {
-          console.warn(err);
-        }
+  // Define the background task for location tracking
+  TaskManager.defineTask(LOCATION_TASK_NAME, async ({data, error}) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    if (data) {
+      // Extract location coordinates from data
+      const {locations} = data;
+      const location = locations[0];
+      if (location) {
+        console.log('Location in background', location.coords);
       }
+    }
+  });
+
+  // Define position state: {latitude: number, longitude: number}
+  const [position, setPosition] = useState(null);
+
+  // Request permissions right after starting the app
+  useEffect(() => {
+    const requestPermissions = async () => {
+      const foreground = await Location.requestForegroundPermissionsAsync();
+      if (foreground.granted)
+        await Location.requestBackgroundPermissionsAsync();
     };
-    requestLocationPermission();
-    return () => {
-      Geolocation.clearWatch(watchID);
-    };
+    requestPermissions();
+    startForegroundUpdate();
+    //startBackgroundUpdate();
   }, []);
 
-  const getOneTimeLocation = () => {
-    setLocationStatus('Getting Location ...');
-    Geolocation.getCurrentPosition(
-      //Will give you the current location
-      position => {
-        setLocationStatus('You are Here');
+  // Start location tracking in foreground
+  const startForegroundUpdate = async () => {
+    // Check if foreground permission is granted
+    const {granted} = await Location.getForegroundPermissionsAsync();
+    if (!granted) {
+      console.log('location tracking denied');
+      return;
+    }
 
-        //getting the Longitude from the location json
-        const currentLongitude = JSON.parse(position.coords.longitude);
+    // Make sure that foreground location tracking is not running
+    foregroundSubscription?.remove();
 
-        //getting the Latitude from the location json
-        const currentLatitude = JSON.parse(position.coords.latitude);
-
-        //Setting Longitude state
-        setCurrentLongitude(currentLongitude);
-
-        //Setting Longitude state
-        setCurrentLatitude(currentLatitude);
-      },
-      error => {
-        setLocationStatus(error.message);
-      },
+    // Start watching position in real-time
+    foregroundSubscription = await Location.watchPositionAsync(
       {
-        enableHighAccuracy: false,
-        timeout: 30000,
-        maximumAge: 1000,
+        // For better logs, we set the accuracy to the most sensitive option
+        accuracy: Location.Accuracy.BestForNavigation,
+      },
+      location => {
+        setPosition(location.coords);
       },
     );
   };
 
-  const subscribeLocationLocation = () => {
-    watchID = Geolocation.watchPosition(
-      position => {
-        //Will give you the location on location change
-
-        setLocationStatus('You are Here');
-
-        //getting the Longitude from the location json
-        const currentLongitude = JSON.parse(position.coords.longitude);
-
-        //getting the Latitude from the location json
-        const currentLatitude = JSON.parse(position.coords.latitude);
-
-        //Setting Longitude state
-        setCurrentLongitude(currentLongitude);
-
-        //Setting Latitude state
-        setCurrentLatitude(currentLatitude);
-      },
-      error => {
-        setLocationStatus(error.message);
-      },
-      {
-        enableHighAccuracy: false,
-        maximumAge: 1000,
-      },
-    );
+  // Stop location tracking in foreground
+  const stopForegroundUpdate = () => {
+    foregroundSubscription?.remove();
+    setPosition(null);
   };
-  console.log(currentLongitude);
-  console.log(currentLatitude);
+
+  // Start location tracking in background
+  const startBackgroundUpdate = async () => {
+    // Don't track position if permission is not granted
+    const {granted} = await Location.getBackgroundPermissionsAsync();
+    if (!granted) {
+      console.log('location tracking denied');
+      return;
+    }
+
+    // Make sure the task is defined otherwise do not start tracking
+    const isTaskDefined = await TaskManager.isTaskDefined(LOCATION_TASK_NAME);
+    if (!isTaskDefined) {
+      console.log('Task is not defined');
+      return;
+    }
+
+    // Don't track if it is already running in background
+    const hasStarted = await Location.hasStartedLocationUpdatesAsync(
+      LOCATION_TASK_NAME,
+    );
+    if (hasStarted) {
+      console.log('Already started');
+      return;
+    }
+
+    await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+      // For better logs, we set the accuracy to the most sensitive option
+      accuracy: Location.Accuracy.BestForNavigation,
+      // Make sure to enable this notification if you want to consistently track in the background
+      showsBackgroundLocationIndicator: true,
+      foregroundService: {
+        notificationTitle: 'Location',
+        notificationBody: 'Location tracking in background',
+        notificationColor: '#fff',
+      },
+    });
+  };
+
+  // Stop location tracking in background
+  const stopBackgroundUpdate = async () => {
+    const hasStarted = await Location.hasStartedLocationUpdatesAsync(
+      LOCATION_TASK_NAME,
+    );
+    if (hasStarted) {
+      await Location.stopLocationUpdatesAsync(LOCATION_TASK_NAME);
+      console.log('Location tacking stopped');
+    }
+  };
+
   return (
     <SafeAreaView style={{flex: 1}}>
-      <TouchableOpacity
-        style={{flex: 1}}
-        onPress={() => {
-          list();
-        }}
-      />
+      {/* <View style={styles.container}>
+        <Text>Longitude: {position?.longitude}</Text>
+        <Text>Latitude: {position?.latitude}</Text> */}
       <View style={[{flex: 3}, styles.container]}>
         <MapView
           style={styles.mapStyle}
@@ -155,18 +150,45 @@ const UsersMapScreen = ({navigation}) => {
           <Marker
             draggable
             coordinate={{
-              latitude: currentLatitude,
-              longitude: currentLongitude,
+              latitude: position?.latitude,
+              longitude: position?.longitude,
             }}
             onDragEnd={e => alert(JSON.parse(e.nativeEvent.coordinate))}
-            title={'Vous êtes ici'}
+            title={
+              'Tu es à : ' + position?.latitude + ' , ' + position?.longitude
+            }
             //description={'This is a description of the marker'}
           />
         </MapView>
+        {/* <View style={styles.separator} />
+      <Button
+        onPress={startForegroundUpdate}
+        title="Start in foreground"
+        color="green"
+      />
+      <View style={styles.separator} />
+      <Button
+        onPress={stopForegroundUpdate}
+        title="Stop in foreground"
+        color="red"
+      />
+      <View style={styles.separator} />
+      <Button
+        onPress={startBackgroundUpdate}
+        title="Start in background"
+        color="green"
+      />
+      <View style={styles.separator} />
+      <Button
+        onPress={stopBackgroundUpdate}
+        title="Stop in foreground"
+        color="red"
+      /> */}
       </View>
     </SafeAreaView>
   );
 };
+
 export default UsersMapScreen;
 
 const mapStyle = [
@@ -249,15 +271,23 @@ const mapStyle = [
     stylers: [{color: '#17263c'}],
   },
 ];
+
 const styles = StyleSheet.create({
   container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
+    flex: 1,
+    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'flex-end',
+    justifyContent: 'center',
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  button: {
+    marginTop: 15,
+  },
+  separator: {
+    marginVertical: 8,
   },
   mapStyle: {
     position: 'absolute',
@@ -273,125 +303,3 @@ const styles = StyleSheet.create({
     left: 30,
   },
 });
-
-// const mapStyle = [
-//   {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-//   {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-//   {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-//   {
-//     featureType: 'administrative.locality',
-//     elementType: 'labels.text.fill',
-//     stylers: [{color: '#d59563'}],
-//   },
-//   {
-//     featureType: 'poi',
-//     elementType: 'labels.text.fill',
-//     stylers: [{color: '#d59563'}],
-//   },
-//   {
-//     featureType: 'poi.park',
-//     elementType: 'geometry',
-//     stylers: [{color: '#263c3f'}],
-//   },
-//   {
-//     featureType: 'poi.park',
-//     elementType: 'labels.text.fill',
-//     stylers: [{color: '#6b9a76'}],
-//   },
-//   {
-//     featureType: 'road',
-//     elementType: 'geometry',
-//     stylers: [{color: '#38414e'}],
-//   },
-//   {
-//     featureType: 'road',
-//     elementType: 'geometry.stroke',
-//     stylers: [{color: '#212a37'}],
-//   },
-//   {
-//     featureType: 'road',
-//     elementType: 'labels.text.fill',
-//     stylers: [{color: '#9ca5b3'}],
-//   },
-//   {
-//     featureType: 'road.highway',
-//     elementType: 'geometry',
-//     stylers: [{color: '#746855'}],
-//   },
-//   {
-//     featureType: 'road.highway',
-//     elementType: 'geometry.stroke',
-//     stylers: [{color: '#1f2835'}],
-//   },
-//   {
-//     featureType: 'road.highway',
-//     elementType: 'labels.text.fill',
-//     stylers: [{color: '#f3d19c'}],
-//   },
-//   {
-//     featureType: 'transit',
-//     elementType: 'geometry',
-//     stylers: [{color: '#2f3948'}],
-//   },
-//   {
-//     featureType: 'transit.station',
-//     elementType: 'labels.text.fill',
-//     stylers: [{color: '#d59563'}],
-//   },
-//   {
-//     featureType: 'water',
-//     elementType: 'geometry',
-//     stylers: [{color: '#17263c'}],
-//   },
-//   {
-//     featureType: 'water',
-//     elementType: 'labels.text.fill',
-//     stylers: [{color: '#515c6d'}],
-//   },
-//   {
-//     featureType: 'water',
-//     elementType: 'labels.text.stroke',
-//     stylers: [{color: '#17263c'}],
-//   },
-// ];
-
-// const styles = StyleSheet.create({
-//   container: {
-//     position: 'absolute',
-//     top: 0,
-//     left: 0,
-//     right: 0,
-//     bottom: 0,
-//     alignItems: 'center',
-//     justifyContent: 'flex-end',
-//   },
-//   mapStyle: {
-//     position: 'absolute',
-//     top: 0,
-//     left: 0,
-//     right: 0,
-//     bottom: 0,
-//   },
-//   switchList: {
-//     flexDirection: 'row-reverse',
-//     position: 'absolute',
-//     top: 100,
-//     left: 30,
-//   },
-// });
-
-// // const styles = StyleSheet.create({
-// //   container: {
-// //     flex: 1,
-// //     backgroundColor: 'white',
-// //     padding: 10,
-// //     alignItems: 'center',
-// //     justifyContent: 'center',
-// //   },
-// //   boldText: {
-// //     fontSize: 25,
-// //     color: 'red',
-// //     marginVertical: 16,
-// //     textAlign: 'center',
-// //   },
-// // });
